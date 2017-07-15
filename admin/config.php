983124bb64e9492d6925a0bc159962cfa2a794e1
hngochanh <hngochanh14@gmail.com>
2017-07-15 23:07:37 +0700

    init proj

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/chodemsv/admin/');
+define('HTTP_CATALOG', 'http://localhost/chodemsv/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/chodemsv/admin/');
+define('HTTPS_CATALOG', 'http://localhost/chodemsv/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/chodemsv/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/chodemsv/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/chodemsv/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/chodemsv/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/chodemsv/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/chodemsv/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/chodemsv/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/chodemsv/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/chodemsv/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/chodemsv/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/chodemsv/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/chodemsv/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'chodemsv');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
